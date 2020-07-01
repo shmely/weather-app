ce4725b101e5aac590f0e2d24365c3ede6809d3d
Alon Yaari <shmely@gmail.com>
2020-07-01 18:12:42 +0300

    Updates

---
+++ precache-manifest.96dc2e64104524805f6d6d7a2c1ee49f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0dc0467f0de142141ab1b2f45eabfb",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "09b78f44fd88e20ec608",
+    "url": "/weather-app/static/css/main.b3e192e1.chunk.css"
+  },
+  {
+    "revision": "f8652b6d79b31bb35f7b",
+    "url": "/weather-app/static/js/2.6bdf324d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.6bdf324d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b78f44fd88e20ec608",
+    "url": "/weather-app/static/js/main.320c87c1.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "dc7bceaa5cca20023e332a65fc9aa2a1",
+    "url": "/weather-app/static/media/berkshireswash-regular.dc7bceaa.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/weather-app/static/media/open-sans.3ed9575d.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/weather-app/static/media/roboto-black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/weather-app/static/media/roboto-bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/weather-app/static/media/roboto-light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/weather-app/static/media/roboto-regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/weather-app/static/media/roboto-thin.321de678.ttf"
+  },
+  {
+    "revision": "65099f98c7cb19b3dac57b15a6a708cf",
+    "url": "/weather-app/static/media/segoe-ui-bold.65099f98.ttf"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/weather-app/static/media/segoe-ui.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/weather-app/static/media/ubuntu.2505bfbd.ttf"
+  }
+]);
